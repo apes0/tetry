d6a2d637a681f46e1cca72346608dc70c8d14fcb
apes0 <borislavkosturski@gmail.com>
2021-09-02 16:25:27 +0300

    add licence to package info! :>

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ with open('requirements.txt') as f:
     requirements = f.read()
 
 __version__ = ''
+__licence__ = ''
+__author__ = ''
+__name__ = ''
 
 with open('tetry/__init__.py') as f:
     for line in f.read().splitlines():
@@ -17,7 +20,8 @@ source = 'https://github.com/apes0/tetry'
 
 setup(
     url=source,
-    name='tetry',
+    name=__name__,
+    license=__licence__,
     project_urls={
         'Source': source,
         'Tracker': f'{source}/issues',
@@ -28,7 +32,7 @@ setup(
     description='tetr.io api wrapper',
     long_description=readme,
     long_description_content_type='text/markdown',
-    author='apes0',
+    author=__author__,
     python_requires='>=3',
     install_requires=requirements
 )
--- tetry/__init__.py
+++ tetry/__init__.py
@@ -1,3 +1,6 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
+__licence__ = 'GPLv3'
+__author__ = 'apes0'
+__name__ = 'tetry'
 
 from . import api, bot
